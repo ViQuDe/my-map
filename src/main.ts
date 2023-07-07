b7c837d3405e57d7013c7676bc334997389a3b34
quentindebruyne <135249721+ViQuDe@users.noreply.github.com>
2023-07-07 16:52:01 +0200

    Update code

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
-let playAudio: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
